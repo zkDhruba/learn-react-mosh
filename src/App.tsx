2dca1166d96a4d12023e3c7b3ace76474519cf9b
Dhruba Karim <dhruba@ussbdpos.com>
2024-04-05 16:05:38 +0600

    Understanding effect hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,20 @@
-import { Children, useState } from "react";
+import { Children, useEffect, useRef, useState } from "react";
 import "./App.css";
-import ExpenseList from "./components/expense-tracker/components/ExpenseList";
-import ExpenseFilter from "./components/expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./components/expense-tracker/components/ExpenseForm";
-import categories from "./components/expense-tracker/categories";
 
 function App() {
-  const [selectedCategory, setSelectedCategory] = useState("");
+  const ref = useRef<HTMLInputElement>(null);
 
-  const [expenses, setExpenses] = useState([
-    { id: 1, description: "abc", amount: 50, category: "Utilities" },
-    { id: 2, description: "bcd", amount: 150, category: "Entertainment" },
-    { id: 3, description: "cda", amount: 250, category: "Utilities" },
-    { id: 4, description: "def", amount: 350, category: "Utilities" },
-  ]);
+  useEffect(() => {
+    if (ref.current) ref.current.focus();
+  });
 
-  const handleOnDelete = (id: number) => {
-    setExpenses(expenses.filter((e) => e.id !== id));
-  };
-
-  const handleOnSelectCategory = (category: string) =>
-    setSelectedCategory(category);
-
-  const visibleExpense = selectedCategory
-    ? expenses.filter((e) => e.category === selectedCategory)
-    : expenses;
+  useEffect(() => {
+    document.title = "My App";
+  });
 
   return (
     <div>
-      <div className="mb-5">
-        <ExpenseForm
-          onSubmit={(expense) =>
-            setExpenses([...expenses, { id: expenses.length + 1, ...expense }])
-          }
-        ></ExpenseForm>
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter
-          onSelectCategory={handleOnSelectCategory}
-        ></ExpenseFilter>
-      </div>
-      <ExpenseList expenses={visibleExpense} onDelete={handleOnDelete} />
+      <input ref={ref} type="text" className="form-control" />
     </div>
   );
 }
