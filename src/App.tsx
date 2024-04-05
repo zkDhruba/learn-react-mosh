4ca89f8b4696a301dfa9d1f8374cf6dc9c09c94e
Dhruba Karim <dhruba@ussbdpos.com>
2024-04-05 11:01:36 +0600

    Expense Form Created

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { Children, useState } from "react";
 import "./App.css";
 import ExpenseList from "./components/expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./components/expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./components/expense-tracker/components/ExpenseForm";
+
+export const categories = ["Groceries", "Utilities", "Entertainment"];
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("");
@@ -26,6 +29,9 @@ function App() {
 
   return (
     <div>
+      <div className="mb-5">
+        <ExpenseForm></ExpenseForm>
+      </div>
       <div className="mb-3">
         <ExpenseFilter
           onSelectCategory={handleOnSelectCategory}
--- src/components/expense-tracker/components/ExpenseFilter.tsx
+++ src/components/expense-tracker/components/ExpenseFilter.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { categories } from "../../../App";
 
 interface Props {
   onSelectCategory: (category: string) => void;
@@ -12,9 +13,11 @@ const ExpenseFilter = ({ onSelectCategory }: Props) => {
         onChange={(event) => onSelectCategory(event.target.value)}
       >
         <option value="">All Categories</option>
-        <option value="Groceries">Groceries</option>
-        <option value="Utilities">Utilities</option>
-        <option value="Entertainment">Entertainment</option>
+        {categories.map((category) => (
+          <option key={category} value={category}>
+            {category}
+          </option>
+        ))}
       </select>
     </div>
   );
