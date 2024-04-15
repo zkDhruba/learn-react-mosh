df4bdd75d81d2cbf88570d484ff70040da9c98f5
Dhruba Karim <dhruba@ussbdpos.com>
2024-04-15 17:41:00 +0600

    loader shown

--- src/App.tsx
+++ src/App.tsx
@@ -12,24 +12,33 @@ interface User {
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setisLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
+    setisLoading(true);
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users", {
         signal: controller.signal,
       })
       .then((res) => {
         setUsers(res.data);
+        setisLoading(false);
       })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setisLoading(false);
       });
   }, []);
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
+      {isLoading && (
+        <div className="text-center">
+          <div className="spinner-border"></div>
+        </div>
+      )}
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
