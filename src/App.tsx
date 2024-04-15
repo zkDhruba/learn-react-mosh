54bac2733b3dd84c539557f01f93ce2f77bfc6c6
Dhruba Karim <dhruba@ussbdpos.com>
2024-04-15 14:44:27 +0600

    handling http error

--- src/App.tsx
+++ src/App.tsx
@@ -10,20 +10,25 @@ interface User {
 
 function App() {
   const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .get<User[]>("https://jsonplaceholder.typicode.com/xusers")
       .then((res) => {
         setUsers(res.data);
-      });
+      })
+      .catch((err) => setError(err.message));
   }, []);
   return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>{user.name}</li>
-      ))}
-    </ul>
+    <>
+      {error && <p className="text-danger">{error}</p>}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
